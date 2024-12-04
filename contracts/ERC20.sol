f5fd43c0f08f50b1307e2aad9c097be3574823d3
mahdi <168580151+mahdiZarrei@users.noreply.github.com>
2024-12-04 11:58:47 +0330

    edit

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity 0.8.26;
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Ether is ERC20 {
